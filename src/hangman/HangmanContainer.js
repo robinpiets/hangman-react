ca5f6b2e67ef3ee0ea548bad3090697caca03b36
Robin Piets <robinpiets@gmail.com>
2017-12-02 20:06:31 +0100

    Basics of hangman container

---
+++ src/hangman/HangmanContainer.js
@@ -0,0 +1,18 @@
+import React, {PureComponent} from 'react'
+import PropTypes from 'prop-types'
+
+class HangmanContainer extends PureComponent {
+	static propTypes = {
+		// ...
+	}
+
+	render() {
+		return (
+			<div class="hangman-container">
+				
+			</div>
+		)
+	}
+}
+
+export default HangmanContainer
